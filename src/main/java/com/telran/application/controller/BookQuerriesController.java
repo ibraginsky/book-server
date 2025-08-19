4ed2a3b1988ac37fe2532b27d9f80f6f23160758
Emily McMullan <emcmulla@redhat.com>
2025-08-18 23:14:46 -0400

    create book class
    Signed-off-by: Emily McMullan <emcmulla@redhat.com>

--- src/main/java/com/telran/application/controller/BookQuerriesController.java
+++ src/main/java/com/telran/application/controller/BookQuerriesController.java
@@ -1,7 +1,8 @@
 package com.telran.application.controller;
 
 import com.telran.application.model.BookModel;
-import dto.Book;
+import com.telran.application.model.dto.Book;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
